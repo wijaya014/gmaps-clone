e9f41f3786cf56df4e27c73417d9a2b8dfe0f0db
wijaya014 <wijaya.agus014@gmail.com>
2021-07-31 09:04:34 +0700

    display map, add current location

---
+++ index.js
@@ -0,0 +1,20 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYWd1czAwNzkxIiwiYSI6ImNrcnIyeGR0ODAzdHQyd213Mm1vOXpxMHkifQ.y_uB64NGg2BOlbJnJ6puZw";
+navigator.geolocation.getCurrentPosition(succesLocation, errorLocation, {
+  enableHighAccuracy: true,
+});
+
+function succesLocation(position) {
+  console.log(position);
+  setupMap([position.coords.longitude, position.coords.latitude]);
+}
+function errorLocation() {}
+
+const setupMap = (center) => {
+  var map = new mapboxgl.Map({
+    container: "map", // container ID
+    style: "mapbox://styles/mapbox/streets-v11", // style URL
+    center: center,
+    zoom: 15, // starting zoom
+  });
+};
